438de6b85688b174a56fa65b13d1d8938c0d1b2c
Hoan <hoandtph46864@fpt.edu.vn>
2024-03-06 23:06:13 +0700

    DEVELOPMENT-1/Add detail order

---
+++ src/main/java/com/example/duantn/Controller/Test1Controller.java
@@ -0,0 +1,11 @@
+package com.example.duantn.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@Controller
+@RequestMapping("/test1")
+public class Test1Controller {
+}
